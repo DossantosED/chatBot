57f31a5c8a32b63118891c55e40e3256be3f8520
Emanuel Dos Santos <57354733+DossantosED@users.noreply.github.com>
2021-07-14 14:53:07 -0300

    agrego webpack y main scss

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import './../scss/main.scss'
+
+function saludar(a,b){
+    console.log(a+b)
+}
+
+saludar(2,3)
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js','.scss']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader","css-loader","sass-loader"],
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'index',
+            template: path.resolve(__dirname, './src/views/index.htm'),
+            filename: 'index.htm',
+        }),
+    ],
+};
