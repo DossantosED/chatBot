f8c684d64dff74f528d36584fd2598be8e2f12de
Emanuel Dos Santos <57354733+DossantosED@users.noreply.github.com>
2021-07-16 17:35:36 -0300

    ahora la IA responde conectandose a una api

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,8 @@ var chatBot = document.querySelector('#chatBot')
 var enterName = document.querySelector('#enterName')
 var divMessage = document.querySelector('#divMessage')
 var messageWelcome = document.querySelector('#messageWelcome')
+var messageIA = document.querySelector('.message-IA')
+var index = 0
 
 send.disabled = true
 next.disabled = true
@@ -34,24 +36,36 @@ name.onkeyup = () => {
 }
 
 send.onclick = () => {
-    sendMessage()
+    insertMessage(name.value, inputMessage.value)
 }
 
 inputMessage.onkeypress = function (e) {
     if (e.keyCode == 13) {
-        sendMessage()
+        insertMessage(name.value, inputMessage.value)
     }
 }
 
-function sendMessage(){
-    if(inputMessage.value.trim() != ""){
-        let newMessage = '<div class="messages message-User"><p><strong>'+name.value+': </strong>'+inputMessage.value+'</p></div>'
-        let cardMessages = document.querySelector('.card-body')
+function insertMessage(entity, message){
+    let newMessage = ""
+    let cardMessages = document.querySelector('.card-body')
+    if(message.trim() != ""){
+        if(entity != 'Adara'){
+            newMessage = '<div class="messages message-User" id="'+entity+'-'+index+'"><p><strong>'+entity+': </strong>'+message+'</p></div>'
+            inputMessage.value = ""
+            iconSend.style.fill = '#707070'
+            send.style.cursor = 'default'
+            send.disabled = true
+            inputMessage.disabled = true
+            setTimeout ( () => connect(entity, message), 800)
+        }else{
+            newMessage = '<div class="messages message-IA" id="'+entity+'-'+index+'"><p><strong>'+entity+': </strong>'+message+'</p></div>'
+        }
         cardMessages.innerHTML += newMessage
-        inputMessage.value = ""
-        inputMessage.focus()
-        iconSend.style.fill = '#707070'
-        send.style.cursor = 'default'
+        var idMessage = document.querySelector("#"+entity+'-'+index)
+        setTimeout ( () => cardMessages.scrollTop = cardMessages.scrollHeight, 800)
+        setTimeout ( () => idMessage.classList.add("active"), 300)
+        index=index+1
+
     }else{
         send.disabled = true
     }
@@ -75,5 +89,30 @@ function showChat(){
         inputMessage.focus()
         setTimeout ( () => divMessage.style.zIndex = 1, 1000)
         messageWelcome.innerHTML = '<strong>Adara:</strong>'+ " Hola "+name.value+'! soy Adara, en que te puedo ayudar?'
+        messageIA.classList.add("active")
     }
+}
+
+function connect(user, message){
+    var url = 'http://127.0.0.1:8000/api/chatBot'
+    var data = {user: user, message: message}
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            "X-CSRF-Token": document.querySelector('input[name=_token]').value
+        }
+    }).then(res => res.json())
+    .catch(error => alert('Opps algo salió mal...'))
+    .then(response => responseIA(response) )
+}
+
+function responseIA(response){
+    insertMessage('Adara', response.response)
+    send.disabled = false
+    setTimeout ( () => inputMessage.disabled = false, 1000)
+    setTimeout ( () => inputMessage.focus(), 1000)
 }
